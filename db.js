23ee35db56e14d3f3cd1689c34ae9bf27b590ed2
lawrenceingithub <lawrencehei@hotmail.com>
2023-12-09 01:34:51 +0800

    first commit

---
+++ db.js
@@ -0,0 +1,5 @@
+let myList = [
+  { id: 1, name: '材料 A', totalprice: 10, totalweight: 5, getPricePerWeight: function() { return this.totalprice / this.totalweight; } },
+  { id: 2, name: '材料 B', totalprice: 15, totalweight: 3, getPricePerWeight: function() { return this.totalprice / this.totalweight; } },
+  { id: 3, name: '材料 C', totalprice: 20, totalweight: 8, getPricePerWeight: function() { return this.totalprice / this.totalweight; } }
+];
